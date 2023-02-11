f8dba6cef2dda5f0bf3e983e19df215aff383ded
magicvlad <magicvlad@gmail.com>
2023-02-11 15:59:19 +0100

    move wadinfo into directory file

--- src/wad.rs
+++ src/wad.rs
@@ -1,7 +1,6 @@
 mod directory;
 pub mod doom_textures;
 pub mod file;
-mod info;
 pub mod map;
 mod patches;
 mod playpal;
--- src/wad/directory.rs
+++ src/wad/directory.rs
@@ -4,7 +4,6 @@ use std::{
     mem,
 };
 
-use super::info::WadInfo;
 #[repr(C, packed)]
 #[derive(Debug, Copy, Clone)]
 pub struct FileLump {
@@ -19,24 +18,36 @@ pub struct WadDirectory {
 
 impl WadDirectory {
     pub fn new(reader: &mut BufReader<File>) -> Self {
-        let info = WadInfo::new(reader);
+        #[repr(C, packed)]
+        #[derive(Clone, Copy)]
+        pub struct WadInfo {
+            pub identification: [u8; 4],
+            pub num_lumps: i32,
+            pub into_table_ofs: i32,
+        }
 
-        let mut directory = WadDirectory {
-            files: Vec::with_capacity(info.num_lumps as usize),
-        };
+        reader.rewind().unwrap();
+
+        let mut buffer = [0u8; mem::size_of::<WadInfo>()];
+        reader.read_exact(&mut buffer).unwrap();
+        let (_, info, _) = unsafe { buffer.align_to::<WadInfo>() };
+        let info = info[0];
+
+        let num_lumps = usize::try_from(info.num_lumps).unwrap();
+        let mut files = Vec::with_capacity(num_lumps);
 
         reader
             .seek(SeekFrom::Start(info.into_table_ofs as u64))
             .unwrap();
 
-        for _ in 0..info.num_lumps {
+        for _ in 0..num_lumps {
             let mut buffer = [0u8; mem::size_of::<FileLump>()];
             reader.read_exact(&mut buffer).unwrap();
             let (_, lump, _) = unsafe { buffer.align_to::<FileLump>() };
-            directory.files.push(lump[0]);
+            files.push(lump[0]);
         }
 
-        directory
+        WadDirectory { files }
     }
 
     pub fn find_section(&self, name: &str, from_index: usize) -> Option<usize> {
